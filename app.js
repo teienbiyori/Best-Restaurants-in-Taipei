47f274fc07637b8148251f16703297e72719fd96
Sandra Sika <shika4days@gmail.com>
2024-01-06 02:09:27 +0800

    build: add bootstrap & style.css static files for style

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express")
+const { engine } = require("express-handlebars")
 const app = express()
 const port = 3000
+const restaurantList = require("./public/jsons/restaurant.json").results
+
+//ask express to hand template engine to express-handlebars
+app.engine(".hbs", engine({extname: ".hbs"}))
+app.set("view engine", ".hbs")
+app.set("views", "./views")
+//using datas stored in specific folder
+//key in "localhost/jsons/restaurant.jsons" could read that data 
+app.use(express.static("public"))
 
 app.get("/", (req,res)=>{
   res.redirect("/restaurant")
 })
 
 app.get("/restaurant", (req, res)=>{
-  res.send("things will get better!")
+  res.render("index", {restaurant: restaurantList})
 })
 
 app.get("/detail/:id", (req, res)=>{
