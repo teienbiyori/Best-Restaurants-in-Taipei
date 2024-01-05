12d0a2bdd4d075cf155d1487eb6f5de1856dfa7b
Sandra Sika <shika4days@gmail.com>
2024-01-05 22:56:24 +0800

    build route & controller setup

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express()
 const port = 3000
 
 app.get("/", (req,res)=>{
-  res.send("keep on going!")
+  res.redirect("/restaurant")
+})
+
+app.get("/restaurant", (req, res)=>{
+  res.send("things will get better!")
+})
+
+app.get("/detail/:id", (req, res)=>{
+  const id = req.params.id
+  res.send(`read detail: ${id}`)
 })
 
 app.listen(port, ()=>{
